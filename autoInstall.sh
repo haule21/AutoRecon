136a641a1f23e9bf2269e31a9fece1aeeb063656
haule21 <sungwoo232@gmail.com>
2025-02-22 20:03:33 +0900

    Installer Modify & ParamSpider -> paramspider

--- autoInstall.sh
+++ autoInstall.sh
@@ -3,13 +3,14 @@
 cd Tools
 
 # install assetfinder
-go get -u github.com/tomnomnom/assetfinder
+go install github.com/tomnomnom/assetfinder@latest
 
 # install findomain
 curl -LO https://github.com/findomain/findomain/releases/latest/download/findomain-linux.zip
 unzip findomain-linux.zip
 chmod +x findomain
 sudo mv findomain /usr/bin/findomain
+rm -f findomain-linux.zip
 
 # install subfinder 
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
@@ -18,7 +19,7 @@ go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install github.com/tomnomnom/httprobe@latest
 
 # install kxss
-go get github.com/Emoe/kxss
+go install github.com/Emoe/kxss@latest
 
 # install paramspider
 git clone https://github.com/devanshbatham/paramspider
@@ -35,11 +36,11 @@ cd ..
 # install crtsh
 git clone https://github.com/0xRyuk/crtsh.git
 cd crtsh
-pip install -r requirement.txt
+pip install -r requirements.txt
 cd ..
 
 # install freq
-go install github.com/takshal/freq@latest
+git clone https://github.com/takshal/freq.git
 
 # install waybackurl
 go install github.com/tomnomnom/waybackurls@latest
